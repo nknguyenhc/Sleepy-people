f8e8fe5b40a57fcbdb169a8effb362114be04037
nknguyenhc <nknguyentdn@gmail.com>
2022-09-10 15:04:37 +0800

    Bullets
    Bullets (ranged attack and sword attack) added

---
+++ sword_attack.gd
@@ -0,0 +1,33 @@
+extends Area2D
+
+
+# Declare member variables here. Examples:
+var player
+var direction
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if direction == 'up':
+		rotation_degrees = -90
+	if direction == 'down':
+		rotation_degrees = -90
+		scale.y = -1
+	if direction == 'right':
+		pass
+	if direction == 'left':
+		scale.x = -1
+	
+	if player == 'player1':
+		set_collision_mask_bit(2, true)
+	if player == 'player2':
+		set_collision_mask_bit(1, true)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_AnimatedSprite_animation_finished():
+	queue_free()
