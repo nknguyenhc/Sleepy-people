e470b9fd522a9f3b16be49f2a9be56220f088749
The Cat <56602118+thecatinbox@users.noreply.github.com>
2022-09-10 21:01:13 +0800

    Change fullscreen mode

--- game.gd
+++ game.gd
@@ -27,7 +27,7 @@ func _process(delta):
 func spawnSlime(): 
 	randomize()
 	var slime = slime_scene.instance() 
-	slime.position.x = rand_range(320, 728)
+	slime.position.x = rand_range(320, 688)
 	if k == 0: 
 		slime.position.y = rand_range(388, 428) 
 		k = 1
