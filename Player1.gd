6b3aaa1815a4c2175563e0b600698ca969101d3b
Eola-Z <sxplayer123@gmail.com>
2022-09-10 14:06:15 +0800

    Added Arrow

--- Player1.gd
+++ Player1.gd
@@ -9,19 +9,20 @@ var input_vector
 var MOVEMENT_SPEED = 100
 var FRICTION = 0.3
 
+var health = 100
+var stock = 3
+var current_gun = "arrow"
+
 var character_state = "spawning"
 
 onready var _animated_sprite = $AnimatedSprite
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	_animated_sprite.play("spawn_left")
 	pass # Replace with function body.
 
 
 func _physics_process(delta):
-	
-	
 	input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
