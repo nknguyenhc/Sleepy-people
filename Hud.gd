425f3a0f0d639cedd5d94242569b756940d74010
Parnikkapore <9170353+parnikkapore@users.noreply.github.com>
2022-09-10 16:45:34 +0800

    Hud: rename stocks to lives

--- Hud.gd
+++ Hud.gd
@@ -15,22 +15,22 @@ func _ready():
 func _process(_delta):
 	update_health(get_parent().get_node("Player1"), get_node("Topbar/A"))
 	update_health(get_parent().get_node("Player2"), get_node("Topbar/B"))
-	update_stocks(get_parent().get_node("Player1"), get_node("Topbar/A"))
-	update_stocks(get_parent().get_node("Player2"), get_node("Topbar/B"))
+	update_lives(get_parent().get_node("Player1"), get_node("Topbar/A"))
+	update_lives(get_parent().get_node("Player2"), get_node("Topbar/B"))
 
 func update_health(player, hud_node: VBoxContainer):
 	var healthbar_node: TextureProgress = hud_node.get_node("Health/Bar")
 	healthbar_node.value = player.health
 
-func update_stocks(player, hud_node: VBoxContainer):
-	var stocks = player.stocks
-	var stocks_node: HBoxContainer = hud_node.get_node("Stocks")
-	var stocks_instance: TextureRect = stocks_node.get_node("Stock")
+func update_lives(player, hud_node: VBoxContainer):
+	var lives = player.lives
+	var lives_node: HBoxContainer = hud_node.get_node("Lives")
+	var lives_instance: TextureRect = lives_node.get_node("Life")
 	
 	# delete all existing nodes
-	for c in stocks_node.get_children():
-		stocks_node.remove_child(c)
+	for c in lives_node.get_children():
+		lives_node.remove_child(c)
 	
 	# and add 'em back
-	for i in range(0, stocks):
-		stocks_node.add_child(stocks_instance.duplicate())
+	for i in range(0, lives):
+		lives_node.add_child(lives_instance.duplicate())
